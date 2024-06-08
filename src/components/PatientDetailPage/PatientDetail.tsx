14f734dd6a07b63dcb9dff54b790174f90e6f791
Brendan <brendanjlee5@gmail.com>
2024-06-08 23:40:57 +0900

    patient entries

--- src/components/PatientDetailPage/PatientDetail.tsx
+++ src/components/PatientDetailPage/PatientDetail.tsx
@@ -1,17 +1,20 @@
 import { useParams } from "react-router-dom";
-import { Patient, Gender } from "../../types";
+import { Patient, Gender, Entry } from "../../types";
 import { useState, useEffect } from "react";
+import patientService from "../../services/patients";
+
+// icons
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
 import TransgenderIcon from "@mui/icons-material/Transgender";
 
-interface PatientDetailProps {
-  patients: Patient[];
-}
-
 interface PatientHeaderProps {
   name: string;
   gender: Gender;
+}
+
+interface EntryItemProps {
+  entry: Entry;
 }
 
 const PatientHeader = ({ name, gender }: PatientHeaderProps) => {
@@ -35,23 +38,50 @@ const PatientHeader = ({ name, gender }: PatientHeaderProps) => {
   );
 };
 
-const PatientDetail = ({ patients }: PatientDetailProps) => {
+const EntryItem = ({ entry }: EntryItemProps) => {
+  return (
+    <>
+      <p>
+        {entry.date} <em>{entry.description}</em>
+      </p>
+      <ul>
+        {entry.diagnosisCodes?.map((code, index) => (
+          <li key={index}>{code}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const PatientDetail = () => {
   const [patient, setPatient] = useState<Patient | undefined>(undefined);
   const { id } = useParams();
 
   useEffect(() => {
-    const currPatient = patients.find((patientEntry) => patientEntry.id === id);
-    setPatient(currPatient);
-  }, [id, patient, patients]);
+    const fetchPatient = async () => {
+      if (id === undefined) {
+        setPatient(undefined);
+        return;
+      }
+      const patient = await patientService.getPatient(id);
+      setPatient(patient);
+    };
+    void fetchPatient();
+  }, [id]);
 
   if (patient === undefined) {
-    return <h1>404 Patient not found</h1>;
+    return <h1>404: Patient not found</h1>;
   }
+  console.log(patient);
   return (
     <div>
       <PatientHeader name={patient.name} gender={patient.gender} />
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+      <h2>Entries</h2>
+      {patient.entries.map((entry) => (
+        <EntryItem key={entry.id} entry={entry} />
+      ))}
     </div>
   );
 };
